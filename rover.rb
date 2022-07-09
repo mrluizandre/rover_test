e861c8b1e6ced0224e97dc1019cccc5895848aff
Andre Costa <mr.luizandre@gmail.com>
2022-07-08 21:52:01 -0300

    Rotate rover

--- rover.rb
+++ rover.rb
@@ -9,4 +9,32 @@ class Rover
     @heading = heading
     @plateau = plateau
   end
+
+  def send(command)
+    analyze_command(command)
+  rescue RuntimeError => e
+    puts e.message
+  end
+
+  def rotate_right
+    @heading = COORDINATES[COORDINATES.find_index(heading) - 3]
+  end
+
+  def rotate_left
+    @heading = COORDINATES[COORDINATES.find_index(heading) - 1]
+  end
+
+  def analyze_command(command)
+    case command
+    when 'L'
+      rotate_left
+    when 'R'
+      rotate_right
+    when 'M'
+      puts 'Move'
+    else
+      raise 'Not valid command'
+    end
+    puts "(#{x},#{y}): #{@heading}"
+  end
 end
